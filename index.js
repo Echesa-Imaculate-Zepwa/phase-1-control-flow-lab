23579912a04c4d24cbc905a59f2e8c4740e8a455
imaculate-echesa <immaculatezepwa18@gmail.com>
2022-08-19 14:48:37 +0300

    Done with  assignment

--- index.js
+++ index.js
@@ -1,11 +1,50 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
 
-function ternaryCheckCity(){
   // Write your code here!
-}
+  function scuberGreetingForFeet(ride){
+    // Write your code here!
+    if (ride <= 400){
+      return 'This one is on me!'
+    } else if (ride > 2000 && ride < 2500){
+      return 'I will gladly take your thirty bucks.'
+    } else {
+      return 'No can do.' 
+    }
+  }
+ 
+  // Write your code here!
+  function ternaryCheckCity(city){
+    return (city == "NYC" ? "Ok, sounds good." : "No go.")
+  }
+ 
 
-function switchOnCharmFromTip(){
+
+
   // Write your code here!
-}
+  function switchOnCharmFromTip(tip){
+    // Write your code here!
+     switch (tip){
+      case "generous":
+        return "Thank you so much.";
+        break;
+      case "not as generous":
+        return "Thank you.";
+        break;
+      default:
+        return "Bye.";
+        break;
+    }
+  }
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+
+
+
+
+  
+  
